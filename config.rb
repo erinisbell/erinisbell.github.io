1705b884284da9ed51c9f8cb70bff12fbfa1c5e0
Erin Isbell <erinmarie.isbell@gmail.com>
2015-10-20 16:52:34 -0500

    fix gh-pages

--- config.rb
+++ config.rb
@@ -55,25 +55,27 @@ set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
-  activate :minify_css
+# For example, change the Compass output style for deployment
+activate :minify_css
 
-  # Minify Javascript on build
-  activate :minify_javascript
+# Minify Javascript on build
+activate :minify_javascript
 
-  # Enable cache buster
-   activate :asset_hash
+# Enable cache buster
+activate :asset_hash
 
-  # Use relative URLs
-   activate :relative_assets
+# Use relative URLs
+activate :relative_assets
 
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
+# Or use a different image path
+# set :http_prefix, "/Content/images/"
 end
 activate :deploy do |deploy|
-  deploy.method = :git
-  end
+deploy.method = :git
+deploy.branch = "master"
+deploy.build_before = true
+end
 activate :blog do |blog|
-  blog.sources = "articles/:year-:month-:day-:title"
-  blog.layout = "article"
+blog.sources = "articles/:year-:month-:day-:title"
+blog.layout = "article"
 end
